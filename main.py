5ba6355ad2e89269edece147860c63730f677d6b
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2024-10-02 16:32:50 -0300

    Luiz carlos 02/10/2024
    - Modificar o codigo retirado a  chave api

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import google.generativeai as genai
 import pyttsx3
 import speech_recognition as sr
 
+# pip install SpeechRecognition
+# pip install pyaudio
+# pip install pyttsx3
+# pip install --upgrade
+# pip install google-generativeai 
+
 def main():
     assistente_falante = True
     ligar_microfone = True
 
-    genai.configure(api_key="AIzaSyAXCVnzZWz04KnBYbR27yQhvbpLlWk3gvw")
+    genai.configure(api_key=" ")
     for m in genai.list_models():
         if 'generateContent' in m.supported_generation_methods:
             print(m.name)
@@ -14,46 +20,49 @@ def main():
     model = genai.GenerativeModel('gemini-pro')
     chat = model.start_chat(history=[])
 
-    ### configura voz
+    ### configuração de voz
     if assistente_falante:
-        engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-        engine.setProperty('rate', 180)  # velocidade 180
+        var = pyttsx3.init()
+        Voz = var.getProperty('voices')
+        var.setProperty('rate', 180)  # velocidade da fala 180
+        
+        bem_vindo = "# Ola seja bem vindo ao Garoto de Progama #"
+        print("")
+        print(len(bem_vindo) * "#")
+        print(bem_vindo)
+        print("  !False it's Funny Because it's True #")
+        print(len(bem_vindo) * "#")
+        print("###   Digite ou fale 'desligar' para encerrar    ###")
+        print("")
 
-        print("\nLista de Vozes - Verifique o número\n")
+        print("\n===Lista de Vozes - Verifique o número===\n")
         voz_portugues = None
 
-        for indice, voz in enumerate(voices):
+        for indice, voz in enumerate(Voz):
             print(indice, voz.name)
             if "Portuguese" in voz.name:  # Procura por voz em português
                 voz_portugues = voz.id  # Armazena a voz em português
 
         if voz_portugues is not None:
-            engine.setProperty('voice', voz_portugues)
+            var.setProperty('voice', voz_portugues)
         else:
             print("Voz em português não encontrada!")
 
     if ligar_microfone:
-        r = sr.Recognizer()
+        pop = sr.Recognizer()
         mic = sr.Microphone()
 
-    bem_vindo = "# Bem Vindo ao Assistente Mil Grau com Gemini AI #"
-    print("")
-    print(len(bem_vindo) * "#")
-    print(bem_vindo)
-    print(len(bem_vindo) * "#")
-    print("###   Digite 'desligar' para encerrar    ###")
-    print("")
+   
 
     while True:
         if ligar_microfone:
             with mic as fonte:
-                r.adjust_for_ambient_noise(fonte)
-                print("Fale alguma coisa (ou diga 'desligar')")
-                audio = r.listen(fonte)
+                pop.adjust_for_ambient_noise(fonte)
+                print("Fale alguma coisa (ou diga 'desligar')" )
+                audio = pop.listen(fonte)
                 print("Enviando para reconhecimento")
                 try:
-                    texto = r.recognize_google(audio, language="pt-BR")
+                    texto = pop.recognize_google(audio, language="pt-BR")
                     print("Você disse: {}".format(texto))
                 except Exception as e:
                     print("Não entendi o que você disse. Erro:", e)
@@ -64,14 +73,14 @@ def main():
         if texto.lower() == "desligar":
             break
 
-        # Verifica se o texto não está vazio antes de enviar
         if texto.strip():
-            response = chat.send_message(texto)
+            texto_Modificado = "Responda de uma maneira sarcastica e sem educação "+ texto
+            response = chat.send_message(texto_Modificado)
             print("Gemini:", response.text, "\n")
 
             if assistente_falante:
-                engine.say(response.text)
-                engine.runAndWait()
+                var.say(response.text)
+                var.runAndWait()
         else:
             print("Nenhuma entrada válida para enviar.")
 
