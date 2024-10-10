da889284256f680825e792fd6ac957c7aaa99984
Unifor-MG <107255451+LuixCarlos00@users.noreply.github.com>
2024-10-09 21:07:36 -0300

    Luiz carlos 09/10/2024
    0 foi adicionado uma interface grafica, porem o esta lento para fazer o download

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from pytubefix import YouTube
 import re
 import os
+import tkinter as tk
+from tkinter import messagebox
 
 def download_video(video_url, output_path=None):
     try:
         if output_path is None:
-            NomeUser = os.path.expanduser("~")
-            output_path = os.path.join(NomeUser, "Videos_Downloads")
+            user_home = os.path.expanduser("~")
+            output_path = os.path.join(user_home, "Videos_Downloads")
 
         output_dir = os.path.dirname(output_path)
 
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-            print(f"output_path criada: {output_dir}")
 
         yt = YouTube(video_url)
+
+        # Verifica se o vídeo está disponível
+        if yt.streams.filter(progressive=True).first() is None:
+            raise Exception("O vídeo não está disponível para download.")
+
         stream = yt.streams.get_highest_resolution()
         stream.download(output_path=output_path)
-        print(f"Vídeo baixado com sucesso e salvo em: {output_path}/{yt.title}")
+        messagebox.showinfo("Sucesso", f"Vídeo baixado com sucesso!\n\nSalvo em: {output_path}\n\nVocê pode encontrar seu vídeo na pasta 'Vídeos_Downloads'.")
     except Exception as e:
-        print(f"Ocorreu um erro ao baixar o vídeo: {e}")
+        messagebox.showerror("Erro", f"Ocorreu um erro ao baixar o vídeo: {e}")
+
+
+
 
 def download_audio(video_url, output_path=None):
     try:
@@ -29,15 +38,13 @@ def download_audio(video_url, output_path=None):
 
         if not os.path.exists(output_path):
             os.makedirs(output_path)
-            print(f"output_path criada: {output_path}")
 
         yt = YouTube(video_url)
         stream = yt.streams.get_audio_only()
-        
         stream.download(output_path=output_path)
-        print(f"Áudio baixado com sucesso e salvo em: {output_path}/{yt.title}.mp3")
+        messagebox.showinfo("Sucesso", f"Áudio baixado com sucesso e salvo em: {output_path}/{yt.title}.mp3")
     except Exception as e:
-        print(f"Ocorreu um erro ao baixar o áudio: {e}")
+        messagebox.showerror("Erro", f"Ocorreu um erro ao baixar o áudio: {e}")
 
 def is_valid_url(url):
     regex = r'^(https?://)?(www\.)?(youtube\.com|youtu\.?be)/.+$'
@@ -45,52 +52,89 @@ def is_valid_url(url):
 
 def download_multiple_videos(urls, download_function):
     for url in urls:
-        url = url.strip()  # Remove espaços em branco
+        url = url.strip()   
         if is_valid_url(url):
             download_function(url)
         else:
-            print(f"URL inválido: {url}")
+            messagebox.showwarning("URL Inválido", f"URL inválido: {url}")
 
-def main():
-    while True:
-        print("Selecione uma opção:")
-        print("1 - Baixar vídeo")
-        print("2 - Baixar áudio")
-        print("3 - Baixar lista de vídeos")
-        print("4 - Baixar lista de áudios a partir de vídeos")
-        print("5 - Encerrar")
-        choice = input("Opção: ")
+def on_download_video():
+    video_url = entry_video.get()
+    if is_valid_url(video_url):
+        download_video(video_url)
+    else:
+        messagebox.showwarning("URL Inválido", "Por favor, insira um link válido do YouTube.")
 
-        if choice == "1":
-            video_url = input("Digite o link do vídeo do YouTube que você deseja baixar: ")
-            if is_valid_url(video_url):
-                download_video(video_url)
-            else:
-                print("URL inválido. Por favor, insira um link válido do YouTube.")
+def on_download_audio():
+    video_url = entry_audio.get()
+    if is_valid_url(video_url):
+        download_audio(video_url)
+    else:
+        messagebox.showwarning("URL Inválido", "Por favor, insira um link válido do YouTube.")
 
-        elif choice == "2":
-            video_url = input("Digite o link do áudio do YouTube que você deseja baixar: ")
-            if is_valid_url(video_url):
-                download_audio(video_url)
-            else:
-                print("URL inválido. Por favor, insira um link válido do YouTube.")
+def on_download_multiple_videos():
+    video_urls = entry_multiple_videos.get()
+    urls_list = video_urls.split(',')
+    download_multiple_videos(urls_list, download_video)
 
-        elif choice == "3":
-            video_urls = input("Digite os links dos vídeos do YouTube que você deseja baixar (separados por vírgula): ")
-            urls_list = video_urls.split(',')
-            download_multiple_videos(urls_list, download_video)
+def on_download_multiple_audios():
+    video_urls = entry_multiple_audios.get()
+    urls_list = video_urls.split(',')
+    download_multiple_videos(urls_list, download_audio)
 
-        elif choice == "4":
-            video_urls = input("Digite os links dos vídeos do YouTube que você deseja baixar como áudios (separados por vírgula): ")
-            urls_list = video_urls.split(',')
-            download_multiple_videos(urls_list, download_audio)
 
-        elif choice == "5":
-            print("Saindo...")
-            break 
+root = tk.Tk()
+root.geometry('500x700')
+root.title("Downloader de Vídeos e Áudios do YouTube")
 
-        else:
-            print("Opção inválida. Por favor, escolha uma opção disponível.")
 
-if __name__ == "__main__":
-    main()
+frame_video = tk.Frame(root)
+frame_video.pack(pady=20)
+
+tk.Label(frame_video, text="Baixar Vídeo").grid(row=0, column=0, sticky="w", padx=5 ) 
+entry_video = tk.Entry(frame_video,   width=75)
+entry_video.grid(row=1, column=0, padx=5 )  
+tk.Button(frame_video, text="Baixar Vídeo", command=on_download_video).grid(row=2, column=0,  sticky="w", padx=5 ) 
+
+
+
+
+frame_audio = tk.Frame(root)
+frame_audio.pack(pady=20)
+
+tk.Label(frame_audio, text="Baixar Áudio").grid(row=0, column=0, sticky="w", padx=5 )
+entry_audio = tk.Entry(frame_audio, width=75)
+entry_audio.grid(row=1, column=0, padx=5 )
+tk.Button(frame_audio, text="Baixar Áudio", command=on_download_audio).grid(row=2, column=0, sticky="w", padx=5 )
+
+
+
+
+frame_multiple_videos = tk.Frame(root)
+frame_multiple_videos.pack(pady=40)
+
+tk.Label(frame_multiple_videos, text="Baixar Múltiplos Vídeos ").grid(row=0, column=0, sticky="w", padx=5,  )
+tk.Label(frame_multiple_videos, text="(Separados os links por vírgula)").grid(row=1, column=0, sticky="w", padx=5,  )
+entry_multiple_videos = tk.Text(frame_multiple_videos, width=57 ,height=4)
+entry_multiple_videos.grid(row=2, column=0, padx=5 )
+tk.Button(frame_multiple_videos, text="Baixar Múltiplos Vídeos", command=on_download_multiple_videos).grid(row=3, column=0, sticky="w", padx=5 )
+
+
+
+
+
+frame_multiple_audios = tk.Frame(root)
+frame_multiple_audios.pack(pady=40)
+
+tk.Label(frame_multiple_audios, text="Baixar Múltiplos Áudios").grid(row=0, column=0, sticky="w", padx=5,  )
+tk.Label(frame_multiple_audios, text="(Separados os links por vírgula)").grid(row=1, column=0, sticky="w", padx=5,  )
+entry_multiple_audios = tk.Text(frame_multiple_audios,  width=57 ,height=4)
+entry_multiple_audios.grid(row=2, column=0, padx=5 )
+tk.Button(frame_multiple_audios, text="Baixar Múltiplos Áudios", command=on_download_multiple_audios).grid(row=3, column=0, sticky="w", padx=5 )
+
+
+
+
+tk.Button(root, text="Sair", command=root.quit).pack(pady=20, side="bottom"  )
+
+root.mainloop()
