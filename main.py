9e2430c711f4777da41872848d71766eb1da80e4
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2024-10-11 23:52:26 -0300

    LuizCarlos 11/10/2024
    -Foi retirado o metodo de transcrição de audio pois estava dando problema

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import subprocess
 import threading
 import tkinter as tk
 from tkinter import messagebox, ttk
-from google.cloud import speech
-import io
-
-# Variável global para controlar o cancelamento
+import whisper
+import sys
+from tkinter import filedialog
+   
 cancel_download = False
+
+
+
 
 def open_folder(path):
     if os.name == 'nt':
         os.startfile(path)
     elif os.name == 'posix':
         subprocess.Popen(['open', path] if sys.platform == 'darwin' else ['xdg-open', path])
+
+
+
+
 
 def download_video(video_url):
     global cancel_download
@@ -52,6 +59,11 @@ def download_video(video_url):
     thread = threading.Thread(target=_download)
     thread.start()
 
+
+
+
+
+
 def download_audio(video_url):
     global cancel_download
     cancel_download = False
@@ -83,6 +95,12 @@ def download_audio(video_url):
     thread = threading.Thread(target=_download)
     thread.start()
 
+
+
+
+
+
+
 def progress_callback(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
@@ -90,9 +108,18 @@ def progress_callback(stream, chunk, bytes_remaining):
     progress['value'] = percentage
     progress_label.config(text=f"{percentage:.2f}%")
 
+
+
+
+
+
 def is_valid_url(url):
     regex = r'^(https?://)?(www\.)?(youtube\.com|youtu\.?be)/.+$'
     return re.match(regex, url) is not None
+
+
+
+
 
 def download_multiple_videos(urls, download_function):
     for url in urls:
@@ -101,6 +128,10 @@ def download_multiple_videos(urls, download_function):
             download_function(url)
         else:
             messagebox.showwarning("URL Inválido", f"URL inválido: {url}")
+
+
+
+
 
 def on_download_video():
     global cancel_download
@@ -113,6 +144,10 @@ def on_download_video():
     else:
         messagebox.showwarning("URL Inválido", "Por favor, insira um link válido do YouTube.")
 
+
+
+
+
 def on_download_audio():
     global cancel_download
     cancel_download = False
@@ -124,6 +159,9 @@ def on_download_audio():
     else:
         messagebox.showwarning("URL Inválido", "Por favor, insira um link válido do YouTube.")
 
+
+
+
 def on_cancel_download():
     global cancel_download
     cancel_download = True
@@ -132,6 +170,11 @@ def on_cancel_download():
     progress_label.config(text="")
     cancel_button.pack_forget()
     cancel_button.config(command=None)
+
+
+
+
+
 
 def on_download_multiple_videos():
     video_urls = entry_multiple_videos.get("1.0", tk.END)
@@ -143,104 +186,87 @@ def on_download_multiple_audios():
     urls_list = video_urls.split(',')
     download_multiple_videos(urls_list, download_audio)
 
-def on_transcribe_audio():
-    audio_filename = entry_transcribe.get()
-    user_home = os.path.expanduser("~")
-    output_path = os.path.join(user_home, "Transcriptions")
-    
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
 
-    audio_path = os.path.join(output_path, audio_filename)
-    
-    if os.path.exists(audio_path):
-        transcribe_audio(audio_path)
-    else:
-        messagebox.showwarning("Arquivo Inválido", "Por favor, insira um caminho válido para o arquivo de áudio.")
 
-def transcribe_audio(file_path):
-    client = speech.SpeechClient()
-    with io.open(file_path, "rb") as audio_file:
-        content = audio_file.read()
 
-    audio = speech.RecognitionAudio(content=content)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="pt-BR",
-    )
-    response = client.recognize(config=config, audio=audio)
-    for result in response.results:
-        print("Transcrição: {}".format(result.alternatives[0].transcript))
+ 
+
+ 
+
+
+
+
 
 def toggle_dark_mode():
-    if root.cget("bg") == "white":
-        root.config(bg="black")
-        dark_mode_button.config(bg="grey", fg="white")
-        for frame in [frame_video, frame_multiple_videos, frame_transcribe]:
-            frame.config(bg="black")
+    if root.cget("bg") == "#1a1a1a":  #escuro
+        root.config(bg="MidnightBlue")   
+        dark_mode_button.config(bg="#007BFF", fg="white")   
+        for frame in [frame_video, frame_multiple_videos,frame_progress]:
+            frame.config(bg="MidnightBlue")
             for widget in frame.winfo_children():
-                widget.config(bg="black", fg="white")
+                widget.config(bg="MidnightBlue", fg="white")
         for widget in root.winfo_children():
-            widget.config(bg="black", fg="white")
-    else:
-        root.config(bg="white")
-        dark_mode_button.config(bg="white", fg="black")
-        for frame in [frame_video, frame_multiple_videos, frame_transcribe]:
-            frame.config(bg="white")
+            widget.config(bg="MidnightBlue", fg="white")
+    else:  # Claro
+        root.config(bg="#1a1a1a")   
+        dark_mode_button.config(bg="#007BFF", fg="black")
+        for frame in [frame_video, frame_multiple_videos,frame_progress]:
+            frame.config(bg="#1a1a1a")
             for widget in frame.winfo_children():
-                widget.config(bg="white", fg="black")
+                widget.config(bg="#1a1a1a", fg="white")
         for widget in root.winfo_children():
-            widget.config(bg="white", fg="black")
+            widget.config(bg="#1a1a1a", fg="white")
 
 root = tk.Tk()
 root.geometry('1000x600')
 root.title("Downloader de Vídeos e Áudios do YouTube")
 
-frame_video = tk.Frame(root)
+root.config(bg="MidnightBlue")  
+frame_video = tk.Frame(root, bg="MidnightBlue")  
 frame_video.pack(pady=10)
 
-tk.Label(frame_video, text="Baixar Vídeo").grid(row=0, column=0, sticky="w", padx=5)
+
+tk.Label(frame_video, text="Baixar Vídeo").grid(row=0, column=0, sticky="w", padx=5, pady=(5, 0))
 entry_video = tk.Entry(frame_video, width=75)
-entry_video.grid(row=1, column=0, padx=5)
-tk.Button(frame_video, text="Baixar Vídeo", command=on_download_video).grid(row=2, column=0, sticky="w", padx=5)
+entry_video.grid(row=1, column=0,  padx=5, pady=5)
+tk.Button(frame_video, text="Baixar Vídeo", command=on_download_video).grid(row=2, column=0, sticky="w",  padx=5, pady=5)
 
-tk.Label(frame_video, text="Baixar Áudio").grid(row=0, column=1, sticky="w", padx=5)
+tk.Label(frame_video, text="Baixar Áudio").grid(row=0, column=2, sticky="w", padx=5, pady=(5, 0))
 entry_audio = tk.Entry(frame_video, width=75)
-entry_audio.grid(row=1, column=1, padx=5)
-tk.Button(frame_video, text="Baixar Áudio", command=on_download_audio).grid(row=2, column=1, sticky="w", padx=5)
+entry_audio.grid(row=1, column=2,  padx=5, pady=5)
+tk.Button(frame_video, text="Baixar Áudio", command=on_download_audio).grid(row=2, column=2, sticky="w",  padx=5, pady=5)
 
-frame_multiple_videos = tk.Frame(root)
+
+
+frame_multiple_videos = tk.Frame(root, bg="MidnightBlue")
 frame_multiple_videos.pack(pady=20)
 
-tk.Label(frame_multiple_videos, text="Baixar Múltiplos Vídeos ").grid(row=0, column=0, sticky="w", padx=5)
-tk.Label(frame_multiple_videos, text="(Separados os links por vírgula)").grid(row=1, column=0, sticky="w", padx=5)
-entry_multiple_videos = tk.Text(frame_multiple_videos, width=57, height=4)
-entry_multiple_videos.grid(row=2, column=0, padx=5)
-tk.Button(frame_multiple_videos, text="Baixar Múltiplos Vídeos", command=on_download_multiple_videos).grid(row=3, column=0, sticky="w", padx=5)
+tk.Label(frame_multiple_videos, text="Baixar Múltiplos Vídeos ").grid(row=0, column=0, sticky="w",  padx=5, pady=5)
+tk.Label(frame_multiple_videos, text="(Separados os links por vírgula)").grid(row=1, column=0, sticky="w",  padx=5, pady=5)
+entry_multiple_videos = tk.Text(frame_multiple_videos, width=56, height=4)
+entry_multiple_videos.grid(row=2, column=0,  padx=5, pady=5)
+tk.Button(frame_multiple_videos, text="Baixar Múltiplos Vídeos", command=on_download_multiple_videos).grid(row=3, column=0, sticky="w",  padx=5, pady=5)
 
-tk.Label(frame_multiple_videos, text="Baixar Múltiplos Áudios").grid(row=0, column=1, sticky="w", padx=5)
-tk.Label(frame_multiple_videos, text="(Separados os links por vírgula)").grid(row=1, column=1, sticky="w", padx=5)
-entry_multiple_audios = tk.Text(frame_multiple_videos, width=57, height=4)
-entry_multiple_audios.grid(row=2, column=1, padx=5)
-tk.Button(frame_multiple_videos, text="Baixar Múltiplos Áudios", command=on_download_multiple_audios).grid(row=3, column=1, sticky="w", padx=5)
+tk.Label(frame_multiple_videos, text="Baixar Múltiplos Áudios ").grid(row=0, column=1, sticky="w",  padx=5, pady=5)
+tk.Label(frame_multiple_videos, text="(Separados os links por vírgula)").grid(row=1, column=1, sticky="w",  padx=5, pady=5)
+entry_multiple_audios = tk.Text(frame_multiple_videos, width=56, height=4)
+entry_multiple_audios.grid(row=2, column=1,  padx=5, pady=5)
+tk.Button(frame_multiple_videos, text="Baixar Múltiplos Áudios", command=on_download_multiple_audios).grid(row=3, column=1, sticky="w",  padx=5, pady=5)
 
-frame_transcribe = tk.Frame(root)
-frame_transcribe.pack(pady=20)
 
-tk.Label(frame_transcribe, text="Transcrever Áudio").grid(row=0, column=0, sticky="w", padx=5)
-entry_transcribe = tk.Entry(frame_transcribe, width=75)
-entry_transcribe.grid(row=1, column=0, padx=5)
-tk.Button(frame_transcribe, text="Transcrever Áudio", command=on_transcribe_audio).grid(row=2, column=0, sticky="w", padx=5)
 
-progress = ttk.Progressbar(root, orient="horizontal", length=500, mode="determinate")
+frame_progress = tk.Frame(root, bg="MidnightBlue")
+frame_progress.pack(pady=10)
+
+progress = ttk.Progressbar(frame_progress, orient="horizontal", length=800, mode="determinate", maximum=100 ) 
 progress.pack(pady=10)
-progress_label = tk.Label(root, text="")
+progress_label = tk.Label(frame_progress, text="")
 progress_label.pack()
 
-cancel_button = tk.Button(root, text="Cancelar", command=on_cancel_download)
 
-dark_mode_button = tk.Button(root, text="Alternar Modo Escuro", command=toggle_dark_mode)
+cancel_button = tk.Button(root, text="Cancelar Download")
+
+dark_mode_button = tk.Button(root, text="Modo Escuro/Claro", command=toggle_dark_mode)
 dark_mode_button.pack(pady=10)
 
 root.mainloop()
